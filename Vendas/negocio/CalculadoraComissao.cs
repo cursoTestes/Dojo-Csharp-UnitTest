f0fd02a0c54991515312eb87b84cb52bfc6a1e0b
CFC <cfc@ufrj.br>
2012-01-11 22:02:10 -0200

    initial commit

---
+++ Vendas/negocio/CalculadoraComissao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vendas.negocio
+{
+    public class CalculadoraComissao
+    {
+        public double calculaComissao(double totalVendas)
+        {
+            return Math.Round(totalVendas * 0.05, 2);
+        }
+    }
+}
